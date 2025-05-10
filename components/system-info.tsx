dda052ce563d7cae0dfa80bec68f336cd77c02ec
root <brunohdseising@gmail.com>
2025-05-10 01:07:04 -0300

    systen info

--- components/system-info.tsx
+++ components/system-info.tsx
@@ -38,11 +38,11 @@ export function SystemInfo({ systemInfo }: SystemInfoProps) {
   } = systemInfo;
 
   return (
-    <Card className="rounded-2xl uppercase border border-border/40 p-6 shadow-sm">
+    <Card className="rounded-2xl  border border-border/40 p-6 shadow-sm">
   <CardHeader className="mb-4">
     <CardTitle className="text-xl font-medium flex items-center gap-2 text-foreground">
-      <Server className="h-5 w-5 text-muted-foreground" />
-      Informações do Sistema
+      <Server className="h-5 w-5  text-muted-foreground" />
+    <p className='uppercase'  >  Informações do Sistema </p>
     </CardTitle>
   </CardHeader>
 
